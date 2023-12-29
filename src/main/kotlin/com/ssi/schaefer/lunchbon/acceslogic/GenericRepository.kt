12beb7627cb68973946462793d628f6e40bcff62
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2023-12-29 21:16:17 +0100

    rollback

--- src/main/kotlin/com/ssi/schaefer/lunchbon/acceslogic/GenericRepository.kt
+++ src/main/kotlin/com/ssi/schaefer/lunchbon/acceslogic/GenericRepository.kt
@@ -17,7 +17,7 @@ class GenericRepository<T: Any> : IGenericRepository<T> {
     constructor(firestore: Firestore, collectionName: String, documentClass: Class<T>) {
         this.collectionName = collectionName
         this.firestore = firestore
-        this.documentClass = documentClassujik
+        this.documentClass = documentClass
         this.collection = firestore.collection(collectionName)
     }
 
