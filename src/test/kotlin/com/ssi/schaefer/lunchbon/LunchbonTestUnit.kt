1f88375afe9b44244bef5bb878899332571ad289
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2024-01-01 21:52:30 +0100

    test

--- src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
+++ src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
@@ -21,6 +21,7 @@ class LunchbonTestUnit {
         assertEquals(userId, lunchbon.userId)
         assertEquals(restaurant, lunchbon.restaurant)
         assertEquals(timestamp, lunchbon.timestamp)
+
         assertEquals(null, lunchbon.timestampAsDateString)
 
         // When
