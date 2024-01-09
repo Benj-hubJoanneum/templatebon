c1fb1277e42bb280d7839426d30fbfacbce359a9
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2024-01-09 17:27:50 +0100

    feature test

--- src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
+++ src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
@@ -18,10 +18,8 @@ class LunchbonTestUnit {
         val lunchbon = Lunchbon(userId, restaurant, timestamp, null)
 
         // Then
-        //da ding
         assertEquals(userId, lunchbon.userId)
         assertEquals(restaurant, lunchbon.restaurant)
-        //fi fi
         assertEquals(timestamp, lunchbon.timestamp)
         assertEquals(null, lunchbon.timestampAsDateString)
 
