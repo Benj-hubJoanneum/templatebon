eca81f0f838c3d568b6be7a4b79d1fa7d9d5a659
Lamprecht Benjamin <benjamin.lamprecht1@edu.fh-joanneum.at>
2024-01-13 12:45:08 +0100

    feat

--- src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
+++ src/test/kotlin/com/ssi/schaefer/lunchbon/LunchbonTestUnit.kt
@@ -8,21 +8,29 @@ class LunchbonTestUnit {
 
     @Test
     fun `create Lunchbon and check formatting`() {
+
+        // params
         val userId = "123"
         val restaurant = "Some Restaurant"
         val timestamp = 1703875770448
         val timeZoneId = "UTC"
 
+        // construct
         val lunchbon = Lunchbon(userId, restaurant, timestamp, null)
 
+        // checks
         assertEquals(userId, lunchbon.userId)
         assertEquals(restaurant, lunchbon.restaurant)
         assertEquals(timestamp, lunchbon.timestamp)
         assertEquals(null, lunchbon.timestampAsDateString)
 
+        // set time
         lunchbon.formatTimeStamp(timeZoneId)
 
+        // temporary temp
         val expectedFormattedDateTime = "29-12-2023 18:49:30"
+
+        // more checks
         assertEquals(expectedFormattedDateTime, lunchbon.timestampAsDateString)
     }
 }
